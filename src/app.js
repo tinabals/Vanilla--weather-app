7a8bc0aaeffb78a1c6ce5f35657aa8f128be27c8
Damilola Balogun <balogundamilola.f@gmail.com>
2022-09-26 22:07:08 +0100

    implemented the correct data for the city name, wind, humidity and temperature

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let cityElement = document.querySelector('#city');
   let temperatureElement = document.querySelector('#temperature');
-  temperatureElement.innerHTML = response.data.main.temp;
+  let description = document.querySelector('#description');
+  let humidityElement = document.querySelector('#humidity');
+  let windElement = document.querySelector('#wind');
+  description.innerHTML = response.data.weather[0].description;
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 }
 let apiKey = 'c6afaa2c3ad098fb65625db7f7ba5be4';
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
