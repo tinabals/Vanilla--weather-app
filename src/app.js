3b0359fae18657111959700a01af74eb19a80f9d
Damilola Balogun <balogundamilola.f@gmail.com>
2022-09-26 22:14:20 +0100

    Started implementing the date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+function formatDate() {
+  return 'Friday 05:00';
+}
 function displayTemperature(response) {
   console.log(response.data);
   let cityElement = document.querySelector('#city');
@@ -5,11 +8,13 @@ function displayTemperature(response) {
   let description = document.querySelector('#description');
   let humidityElement = document.querySelector('#humidity');
   let windElement = document.querySelector('#wind');
+  let dateElement = document.querySelector('#date');
   description.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.dt);
 }
 let apiKey = 'c6afaa2c3ad098fb65625db7f7ba5be4';
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
