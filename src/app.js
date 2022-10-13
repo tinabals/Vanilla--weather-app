7c70b5b362d7dab235ff0a729471bc78e893efd6
Damilola Balogun <balogundamilola.f@gmail.com>
2022-10-13 22:59:53 +0100

    Display forecast Data using the api

--- src/app.js
+++ src/app.js
@@ -21,36 +21,47 @@ function formatDate(timestamp) {
   return `${days[day]}  ${hour}:${minutes}`;
 }
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector('#forecast');
 
-  let days = ['Thu', 'Fri', 'Sat', 'Sun'];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-      <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
-        <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
-        </div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+    <div class="col-2">
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max"> ${Math.round(
+          forecastDay.temp.max
+        )}° </span>
+      <span class="weather-forecast-temperature-min"> ${Math.round(
+        forecastDay.temp.min
+      )}° </span>
       </div>
-  `;
+    </div>
+`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
-
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+  return days[day];
+}
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = '8c48afa47a9a9c24f3500c7039d50aaa';
@@ -96,6 +107,7 @@ function hndleSubmit(event) {
 let form = document.querySelector('#search-form');
 
 form.addEventListener('submit', hndleSubmit);
+
 function convertTemp(event) {
   event.preventDefault();
   let temperature = document.querySelector('#temperature');
@@ -120,4 +132,4 @@ celsius.addEventListener('click', getCelsiusTemp);
 
 search('New York');
 
-displayForecast();
+// displayForecast();
