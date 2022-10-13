b64a79473c5eaf2bd678147674ead70d34a7d3ab
Damilola Balogun <balogundamilola.f@gmail.com>
2022-10-13 22:34:33 +0100

    integrated the forecast one call API

--- src/app.js
+++ src/app.js
@@ -20,7 +20,8 @@ function formatDate(timestamp) {
   let day = date.getDay();
   return `${days[day]}  ${hour}:${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector('#forecast');
 
   let days = ['Thu', 'Fri', 'Sat', 'Sun'];
@@ -50,6 +51,14 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = '8c48afa47a9a9c24f3500c7039d50aaa';
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector('#city');
   let temperatureElement = document.querySelector('#temperature');
@@ -70,6 +79,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute('alt', response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = 'c6afaa2c3ad098fb65625db7f7ba5be4';
